db0d5d0454ea111397030983cd99cfb477789422
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2011-12-17 13:57:44 +1100

    Started expeimenting with partial loading to exploit DRY.

--- src/main.rb
+++ src/main.rb
@@ -39,11 +39,21 @@ class MyApp < Sinatra::Base
     JSON.generate(@datasets.keys)
   end
 
-
-
   get '/dataset/:name', :provides=>"json" do
     content_type :json
     JSON.generate(@datasets.keys)
   end
+
+  get '/test' do
+    @title = "Awsome test page"
+    @loader = "/js/test2.js"
+    haml :'test.html'
+  end
+
+helpers do
+  def partial(page, options={})
+    haml page.to_sym, options.merge!(:layout => false)
+  end
+end
 
 end
