bb482de9e815b9677ffc4696dee1a1cd40d0c51d
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2011-12-18 13:51:18 +1100

    Added the Three.js rendering engine to /cube.

--- public/js/cube_loader.js
+++ public/js/cube_loader.js
@@ -1,5 +1,9 @@
+/**
+ * Load reqired scripts that install themselves globally.
+ */
 $LAB
-    .script("/js/modernizr.js").wait()
-    .script("/js/require.js").wait()
+    .script("/js/modernizr.js")
+    .script("/js/require.js")
+    .script("/js/Three.js")
     .script("/js/dust-full-0.3.0.js").wait()
     .script("/js/cube.js");
